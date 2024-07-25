15240a54d03610f07844f1db56a79c3a3d107ea8
Umut Yüksel <umtyuksell@icloud.com>
2024-07-25 19:55:00 +0300

    Initial Commit

---
+++ Trakster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trakster
+//
+//  Created by Umut Yüksel on 25.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
